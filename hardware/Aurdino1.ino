d25392de124338e5f420b769b8e3a6999191034f
Hridyesh-07 <hridyeshb8@gmail.com>
2025-09-15 12:56:14 +0530

    Update Aurdino1.ino
    started adding comm to aurdino

--- hardware/Aurdino1.ino
+++ hardware/Aurdino1.ino
@@ -20,3 +20,13 @@ char keys[ROWS][COLS] = {
 byte rowPins[ROWS] = {11, 12, 13, A1};
 byte colPins[COLS] = {A2, A3, A4, A5};
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+// Serial comm to UNO2
+SoftwareSerial mySerial(6, 7); // RX, TX (UNO1_TX=D7 → UNO2_RX)
+
+String inputTime = "";
+unsigned long setTime[NUM_SERVOS] = {0};
+unsigned long startTime[NUM_SERVOS] = {0};
+bool timeSet[NUM_SERVOS] = {false};
+bool servoRun[NUM_SERVOS] = {false};
+
